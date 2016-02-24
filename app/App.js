3a463faec106bb37b59e9408bc5d7d6568ade1aa
illl48 <mruncanny@gmail.com>
2016-02-24 00:09:29 -0800

    switch to fetch api

--- app/App.js
+++ app/App.js
@@ -5,19 +5,13 @@ import store from './store';
 import actions from './actions';
 
 import OrigDest  from './components/origDest';
-import Display from './components/display';
 
 export class App extends Component {
-	componentWillMount() {
-		
-	}
-    
 	render() {
 		return (
 			<Provider store={store}>
                 <div>
                     <OrigDest />
-                    <Display />
                 </div>
 			</Provider>
 		);
